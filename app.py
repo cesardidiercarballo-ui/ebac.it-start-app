4c71a5a1858af8b3bbe17392a898988be1402c1d
cesardidiercarballo-ui <cesardidier.carballo@gmail.com>
2026-09-03 18:24:22 -0600

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, make_response, redirect, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+@app.route('/nueva-tarea', methods=['GET', 'POST'])
+def nueva_tarea():
+    if request.method == 'POST':
+        nombre = request.form['title']
+        #return redirect("/tareas")
+        tareas = request.cookies.get('tareas', "")
+        #Ej: cookies: tareas=Hacer ejercicio,Leer,Escribir
+        lista_tareas = tareas.split(",") if tareas else []
+        #Ej: lista_tareas = ["Hacer ejercicio", "Leer", "Escribir"]
+        lista_tareas.append(nombre)
+        response = make_response(redirect("/tareas"))
+        #Ej:cookies: tareas= "Hacer ejercicio,Leer,Escribir,Nueva tarea"
+        response.set_cookie('tareas', ",".join(lista_tareas))
+        return response
+    return render_template("formulario.html")
+
 @app.route("/tareas")
 def mostrar_tareas():
-    tareas = [
+    tareas = request.cookies.get('tareas', "")
+    lista_tareas = tareas.split(",") if tareas else []
+    return render_template("tareas.html", tareas=lista_tareas)
     {   
             "nombre": "Hacer ejercicio",
             "estado": "pendiente",
